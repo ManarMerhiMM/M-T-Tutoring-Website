b963626f67205554733298e3e5ba55f44706c29f
ManarMerhiMM <manarmerhi80@gmail.com>
2025-02-26 00:26:26 +0200

    Course Details page completely done; Only functionality left to do is to suggest similar courses, check out the comments in courseDetails.js to know how you can test it

--- JS/courseDetails.js
+++ JS/courseDetails.js
@@ -1,14 +1,80 @@
+// localStorage.setItem("curCourseName", "Calculus I"); Set the course manually over here to test the page's functionality
+// localStorage.setItem("lastPage", "index.html"); set the last page over here so you can test the functionality of the back button or to see that the tutor is taken to the last page after bumping
+document.title = `M & T's ${localStorage.getItem("curCourseName")}`;
+
 let courseName = localStorage.getItem("curCourseName");
-let courseTutor = localStorage.getItem("curCourseTutor");
-let coursePrice = localStorage.getItem("curCoursePrice");
-let isCertifiedCourse = localStorage.getItem("curCourseCertified");
-let courseDescription = localStorage.getItem("curCourseDescription");
+let courseTutor;
+let coursePrice;
+let isCertified;
+let courseDescription;
 let lastPagePath = localStorage.getItem("lastPage");
 
-document.title = `M & T's ${courseName}`;
+let Courses;
 
-document.getElementById("backAnchor").addEventListener("click", (event)=>{
+async function updateCoursesFromDB() {
+    try {
+        const response = await fetch('JS/database.json');
+        const data = await response.json();
+        Courses = data.courses;
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+
+    for (let i = 0; i < Courses.length; i++) {
+        if (Courses[i].name == courseName) {
+            courseTutor = Courses[i].tutor;
+            coursePrice = Courses[i].price;
+            courseDescription = Courses[i].description;
+            isCertified = Courses[i].certified;
+            break;
+        }
+    }
+
+
+    document.getElementById("courseDescription").textContent = courseDescription;
+    document.getElementById("tutorName").textContent = courseTutor;
+    document.getElementById("coursePrice").textContent = coursePrice;
+    document.getElementById("courseTitle").textContent = courseName;
+
+    if (isCertified) {
+        document.getElementById("isCertified").textContent = "✔";
+        document.getElementById("isCertified").style.color = "rgb(28, 218, 54)";
+    }
+    else {
+        document.getElementById("isCertified").textContent = "✘";
+        document.getElementById("isCertified").style.color = "rgb(218, 28, 28)";
+    }
+}
+
+updateCoursesFromDB();
+
+if(localStorage.getItem("LoggedIn") == "true"){
+    if(localStorage.getItem("curAccountType") == "tutor"){
+        document.getElementById("enrollBtn").textContent = "Bump";
+    }
+}
+
+document.getElementById("backAnchor").addEventListener("click", (event) => {
     event.preventDefault();
 
     window.location.href = lastPagePath;
-})
+});
+
+
+document.getElementById("enrollBtn").addEventListener("click", ()=>{
+    if(localStorage.getItem("LoggedIn") == "true"){
+        if(localStorage.getItem("curAccountType") == "tutor"){
+            alert(`Successfully bumped M & T's ${courseName}!`);
+            window.location.href = lastPagePath;
+        }
+        else{
+            if(confirm(`Are you sure you want to enroll in M & T's ${courseName}?`)){
+                window.location.href = "checkout.html";
+            }
+        }
+    }
+    else{
+        alert("A guest cannot enroll in a course, you must log in first!");
+        window.location.href = "login.html";
+    }
+});
