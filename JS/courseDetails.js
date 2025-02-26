e0070c00e73d49f2712cd09f90ce4bd6b2859bf3
ManarMerhiMM <manarmerhi80@gmail.com>
2025-02-26 20:58:21 +0200

    courseDetails page done; only responsiveness is left

--- JS/courseDetails.js
+++ JS/courseDetails.js
@@ -1,4 +1,4 @@
-// localStorage.setItem("curCourseName", "Linear Algebra"); set the course
+// localStorage.setItem("curCourseName", "Cloud Computing");
 // localStorage.setItem("lastPage", "index.html"); set the last page over here so you can test the functionality of the back button or to see that the tutor is taken to the last page after bumping
 document.title = `M & T's ${localStorage.getItem("curCourseName")}`;
 
@@ -8,11 +8,17 @@ let coursePrice;
 let isCertified;
 let courseDescription;
 let studentsOfCourse;
+let courseCategories;
 let lastPagePath = localStorage.getItem("lastPage");
 
 let Courses;
 
-async function updateCoursesFromDB() {
+function getRandomItem(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+async function prepCoursePageUsingJSON() {
     try {
         const response = await fetch('JS/database.json');
         const data = await response.json();
@@ -28,6 +34,7 @@ async function updateCoursesFromDB() {
             courseDescription = Courses[i].description;
             isCertified = Courses[i].certified;
             studentsOfCourse = Courses[i].takenBy;
+            courseCategories = Courses[i].category;
             break;
         }
     }
@@ -47,20 +54,41 @@ async function updateCoursesFromDB() {
         document.getElementById("isCertified").style.color = "rgb(218, 28, 28)";
     }
 
-    if(localStorage.getItem("LoggedIn") == "true"){
-        if(localStorage.getItem("curAccountType") == "tutor"){
-            if(localStorage.getItem("curUsername") == courseTutor){
+    
+
+    if (courseCategories.includes("Tech and Development")) {
+        document.getElementById("courseCard").style.backgroundImage = `url('MEDIA/courseImages/TechAndDev.webp')`;
+    }
+    else if (courseCategories.includes("Mathematics")) {
+        document.getElementById("courseCard").style.backgroundImage = `url('MEDIA/courseImages/Mathematics.jpg')`;
+    }
+    else if (courseCategories.includes("Science and Engineering")) {
+        document.getElementById("courseCard").style.backgroundImage = `url('MEDIA/courseImages/ScienceAndEngineering.jpg')`;
+    }
+    else if (courseCategories.includes("Business and Management")) {
+        document.getElementById("courseCard").style.backgroundImage = `url('MEDIA/courseImages/BusinessAndManagement.webp')`;
+    }
+    else if (courseCategories.includes("Biology and Biomedical")) {
+        document.getElementById("courseCard").style.backgroundImage = `url('MEDIA/courseImages/BiologyAndBiomedical.jpg')`;
+    }
+    else {
+        courseImgtoAdd.src = "MEDIA/courseImages/HealthAndMedicine.jpg";
+    }
+
+    if (localStorage.getItem("LoggedIn") == "true") {
+        if (localStorage.getItem("curAccountType") == "tutor") {
+            if (localStorage.getItem("curUsername") == courseTutor) {
                 document.getElementById("enrollBtn").textContent = "Taught ✔";
                 document.getElementById("enrollBtn").disabled = true;
             }
-            else{
+            else {
                 document.getElementById("enrollBtn").textContent = "Recommend";
             }
-            
+
         }
-        else{
-            for(let i = 0; i < studentsOfCourse.length; i++){
-                if(studentsOfCourse[i] == localStorage.getItem("curUsername")){
+        else {
+            for (let i = 0; i < studentsOfCourse.length; i++) {
+                if (studentsOfCourse[i] == localStorage.getItem("curUsername")) {
                     document.getElementById("enrollBtn").textContent = "Enrolled ✔";
                     document.getElementById("enrollBtn").disabled = true;
                     break;
@@ -68,9 +96,61 @@ async function updateCoursesFromDB() {
             }
         }
     }
+    let courseCounter = 0;
+    let j = 0;
+    while (courseCounter < 6 && j < courseCategories.length) {
+        for (let i = 0; i < Courses.length; i++) {
+            if (Courses[i].category.includes(courseCategories[j]) && Courses[i].name != courseName) {
+                let courseToAdd = document.createElement("div");
+                courseToAdd.classList.add("suggestedCourse");
+                let courseImgtoAdd = document.createElement("img");
+                let courseHeadertoAdd = document.createElement("h3");
+                courseHeadertoAdd.textContent = Courses[i].name;
+                let courseBtntoAdd = document.createElement("button");
+                courseBtntoAdd.classList.add("viewBtns");
+                courseBtntoAdd.textContent = "View Course";
+                courseToAdd.appendChild(courseHeadertoAdd);
+                courseToAdd.appendChild(courseImgtoAdd);
+                courseToAdd.appendChild(courseBtntoAdd);
+
+                if (Courses[i].category.includes("Tech and Development")) {
+                    courseImgtoAdd.src = "MEDIA/courseImages/TechAndDev.webp";
+                }
+                else if (Courses[i].category.includes("Mathematics")) {
+                    courseImgtoAdd.src = "MEDIA/courseImages/Mathematics.jpg";
+                }
+                else if (Courses[i].category.includes("Science and Engineering")) {
+                    courseImgtoAdd.src = "MEDIA/courseImages/ScienceAndEngineering.jpg";
+                }
+                else if (Courses[i].category.includes("Business and Management")) {
+                    courseImgtoAdd.src = "MEDIA/courseImages/BusinessAndManagement.webp";
+                }
+                else if (Courses[i].category.includes("Biology and Biomedical")) {
+                    courseImgtoAdd.src = "MEDIA/courseImages/BiologyAndBiomedical.jpg";
+                }
+                else {
+                    courseImgtoAdd.src = "MEDIA/courseImages/HealthAndMedicine.jpg";
+                }
+
+                document.getElementById("suggestedCoursesContainer").appendChild(courseToAdd);
+                courseCounter++;
+                if (courseCounter >= 6) {
+                    break;
+                }
+            }
+        }
+        j++;
+    }
+
+    document.querySelectorAll(".viewBtns").forEach(btn => {
+        btn.addEventListener("click", (event) => {
+            localStorage.setItem("curCourseName", event.target.previousElementSibling.previousElementSibling.textContent);
+            window.location.href = "courseDetails.html";
+        });
+    });
 }
 
-updateCoursesFromDB();
+prepCoursePageUsingJSON();
 
 
 
@@ -81,20 +161,24 @@ document.getElementById("backAnchor").addEventListener("click", (event) => {
 });
 
 
-document.getElementById("enrollBtn").addEventListener("click", ()=>{
-    if(localStorage.getItem("LoggedIn") == "true"){
-        if(localStorage.getItem("curAccountType") == "tutor"){
+document.getElementById("enrollBtn").addEventListener("click", () => {
+    if (localStorage.getItem("LoggedIn") == "true") {
+        if (localStorage.getItem("curAccountType") == "tutor") {
             alert(`Successfully recommended M & T's ${courseName}!`);
             window.location.href = lastPagePath;
         }
-        else{
-            if(confirm(`Are you sure you want to enroll in M & T's ${courseName}?`)){
+        else {
+            if (confirm(`Are you sure you want to enroll in M & T's ${courseName}?`)) {
                 window.location.href = "checkout.html";
             }
         }
     }
-    else{
+    else {
         alert("A guest cannot enroll in a course, you must log in first!");
         window.location.href = "login.html";
     }
 });
+
+
+
+
