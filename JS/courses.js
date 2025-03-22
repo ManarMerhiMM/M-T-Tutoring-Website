ca1e3e284e2e86ba0cc038ea7116e0acd985fe6c
malekshibli <malekshibli5@gmail.com>
2025-03-22 11:46:46 +0200

    The courses page table is removed

--- JS/courses.js
+++ JS/courses.js
@@ -1,37 +1,4 @@
-document.addEventListener("DOMContentLoaded", loadCourses);
 
-async function loadCourses() {
-    try {
-        const response = await fetch('JS/database.json'); 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        const courses = data.courses;
-        const tableBody = document.querySelector("#coursesTable tbody");
-        tableBody.innerHTML = ""; 
-
-        courses.forEach(course => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td>${course.name}</td>
-                <td>${course.category.join(", ")}</td>
-                <td>${course.tutor}</td>
-                <td>${course.price}</td>
-                <td>${course.takenBy.length}</td>
-                <td class="${course.certified ? 'certified-yes' : 'certified-no'}">
-                    ${course.certified ? "✅ Yes" : "❌ No"}
-                </td>
-                <td>${course.description}</td>
-            `;
-            tableBody.appendChild(row);
-        });
-
-    } catch (error) {
-        console.error("Error loading courses:", error);
-    }
-}
 
 
 if (!localStorage.getItem("LoggedIn")) {
